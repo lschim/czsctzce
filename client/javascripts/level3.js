4c8956c08e46b84ee7434905d6add7ecb2880263
Louis Schimchowitsch <schim.louis@gmail.com>
2017-06-25 22:22:24 -0400

    Yep for level 3 in rush mode

--- client/javascripts/level3.js
+++ client/javascripts/level3.js
@@ -1,33 +1,41 @@
 $(function () {
 
-  let nbrLetters = 4
-  let intervalTimer = 5000 // Add letters every x ms
-  let timeCross = 2000 // ms to cross screen
+  let nbrLetters
+  const nbrLettersInit = 10
+  let timeoutLettersValue // Add letters every x ms
   let timeoutLetters // Returned by setInterval
-  let timeoutTick
+  const timeoutLettersValueInit = 1000
+  let timeoutTick // Returned by setInterval
+  let timeoutTickValue
+  const timeoutTickValueInit = 100
 
 
   function resetLevel3 () {
-    $('#letters').html('____ _\'______________!')
-    nbrLetters = 4
-    intervalTimer = 5000
-    timeCross = 2000
+    $('#correctLetters').html('____ _\'______________!')
+    nbrLetters = nbrLettersInit
+    timeoutLettersValue = timeoutLettersValueInit
+    timeoutTickValue = timeoutTickValueInit
   }
 
   function startLevel3 () {
-    timeoutLetters = setInterval(addLetters, intervalTimer)
-    timeoutTick = setInterval(moveLetters, 10)
+    timeoutLetters = setInterval(addLetters, timeoutLettersValue)
+    timeoutTick = setInterval(moveLetters, timeoutTickValue)
   }
 
   function moveLetters () {
-    const width = $('#letters').width
-    const length = $('#letters').length
-    $('.letter').each((index) => {
-      let offset = $(this).offset()
-      let newOffset = {top: offset.top + parseInt($(this).attr('data-diry')), left: offset.left + parseInt($(this).attr('data-dirx'))}
-      $(this).offset(newOffset)
-      if (newOffset.top < 0 || newOffset.top > length || newOffset.left < 0 || newOffset.left > width) {
-        $(this).remove()
+    const width = $('#letters').width()
+    const height = $('#letters').height()
+    const offsetParent = $('#letters').offset()
+
+    $('.letter').each((index, element) => {
+      let test = $(element)
+      let offset = test.offset()
+      let position = {top: offset.top - offsetParent.top, left: offset.left - offsetParent.left}
+      let newPosition = {top: position.top + parseInt(test.attr('data-diry')), left: position.left + parseInt(test.attr('data-dirx'))}
+      let newOffset = {top: newPosition.top + offsetParent.top, left: newPosition.left + offsetParent.left}
+      test.offset(newOffset)
+      if (newPosition.top < 0 || newPosition.top > height || newPosition.left < 0 || newPosition.left > width) {
+        test.remove()
       }
     })
   }
@@ -41,6 +49,7 @@ $(function () {
   function generateLetter () {
     let dirx = getRandDir()
     let diry = getRandDir()
+    let offsetParent = $('#letters').offset()
     while (dirx === 0 && diry === 0) {
       dirx = getRandDir()
       diry = getRandDir()
@@ -48,17 +57,30 @@ $(function () {
     let char = getAsciiCodeOfLetter()
 
     let newLetter = $('<div class="letter" data-dirx="' + dirx + '" ' + 'data-diry=' + diry + '">' + String.fromCharCode(char) + '</div>')
+    newLetter.on('click', clickOnLetter)
     $('#letters').append(newLetter)
-    newLetter.offset = generateStartingOffset(dirx, diry)
+    let position = generateStartingPosition(dirx, diry)
+    newLetter.offset({top: position.top + offsetParent.top, left: position.left + offsetParent.left})
   }
 
-  function generateStartingOffset (dirx, diry) {
-    const width = $('#letters').width
-    const length = $('#letters').length
+  function clickOnLetter (e) {
+    $(e.target).remove()
+    nbrLetters += 2
+    clearInterval(timeoutLetters)
+    //timeoutLettersValue -= 200
+    timeoutLetters = setInterval(addLetters, timeoutLettersValue)
+    clearInterval(timeoutTick)
+    timeoutTickValue /= 1.1
+    timeoutTick = setInterval(moveLetters, timeoutTickValue)
+  }
+
+  function generateStartingPosition (dirx, diry) {
+    const width = $('#letters').width()
+    const height = $('#letters').height()
     if (dirx === 0) {
       if (diry < 0) {
         // It goes up
-        return {top: length, left: getRandPx(width)}
+        return {top: height, left: getRandPx(width)}
       } else {
         // It goes down
         return {top: 0, left: getRandPx(width)}
@@ -68,10 +90,10 @@ $(function () {
     if (diry === 0) {
       if (dirx < 0) {
         // It goes left
-        return {top: getRandPx(length), left: width}
+        return {top: getRandPx(height), left: width}
       } else {
         // It goes right
-        return {top: getRandPx(length), left: 0}
+        return {top: getRandPx(height), left: 0}
       }
     }
 
@@ -79,18 +101,18 @@ $(function () {
     if (dirx < 0) {
       if (diry < 0) {
         // It goes up and left
-        return {top: length - (corner * getRandPx(length)), left: width - ((1 - corner) * getRandPx(width))}
+        return {top: height - (corner * getRandPx(height)), left: width - ((1 - corner) * getRandPx(width))}
       } else {
         // It goes down and left
-        return {top: corner * getRandPx(length), left: width - ((1 - corner) * getRandPx(width))}
+        return {top: corner * getRandPx(height), left: width - ((1 - corner) * getRandPx(width))}
       }
     } else {
       if (diry < 0) {
         // It goes up and right
-        return {top: length - (corner * getRandPx(length)), left: (1 - corner) * getRandPx(width)}
+        return {top: height - (corner * getRandPx(height)), left: (1 - corner) * getRandPx(width)}
       } else {
         // It goes down and right
-        return {top: corner * getRandPx(length), left: (1 - corner) * getRandPx(width)}
+        return {top: corner * getRandPx(height), left: (1 - corner) * getRandPx(width)}
       }
     }
   }
@@ -100,8 +122,15 @@ $(function () {
   }
 
   function getRandDir () {
-    return Math.floor(Math.random() * 5) - 2 // Between -2 and 2
+    let val = (Math.floor(Math.random() * 11) - 5) // Between -5 and 5
+    if (val < 0) {
+      return val - 4
+    } else if (val > 0) {
+      return val + 4
+    }
+    return val
   }
+
 
   function getAsciiCodeOfLetter () {
     return Math.floor(Math.random() * 26) + 97 // Between 97 and 122
