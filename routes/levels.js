e097229f9c544f977f92f8889d002d91eadd6541
Louis Schimchowitsch <schim.louis@gmail.com>
2017-06-26 17:08:15 -0400

    Improve lvl 3

--- routes/levels.js
+++ routes/levels.js
@@ -1,12 +1,27 @@
-var express = require('express')
-var router = express.Router()
+let express = require('express')
+let router = express.Router()
 const RADIUS = 10
 const RESPONSE = [
 {t: 11, x: 133, y: 115},
 {t: 12, x: 133, y: 123},
 {t: 13, x: 135, y: 123},
 {t: 14, x: 130, y: 118}]
-var currentLvl = 1
+let currentLvl = 1
+const defaultStateLvl3 = '____ _\'______________!'
+
+let currentStateLvl3 = defaultStateLvl3
+const lettersDefault = {
+  "n": {pos: [0, 16, 17, 19], played: false},
+  "o": {pos: [1, 15, 18], played: false},
+  "u": {pos: [2], played: false},
+  "s": {pos: [3, 12, 13, 20], played: false},
+  "m": {pos: [5, 8], played: false},
+  "i": {pos: [7, 14], played: false},
+  "p": {pos: [9], played: false},
+  "r": {pos: [10], played: false},
+  "e": {pos: [11], played: false}
+ }
+const lettersState //TODO copy lettersDefault
 /* GET users listing. */
 router.get('/level1', function (req, res, next) {
   res.render('level1')
@@ -77,5 +92,64 @@ router.get('/level3', function (req, res, next) {
   }
 })
 
+router.post('/level3/reset', function (req, res, next) {
+  resetLvl3()
+  res.setHeader('Content-Type', 'application/json')
+  res.send(JSON.stringify({result: 'ok'}))
+})
+
+router.get('/level3/state', function (req, res, next) {
+  res.setHeader('Content-Type', 'application/json')
+  res.send(JSON.stringify({state: currentStateLvl3}))
+})
+
+router.post('/level3/letter', function (req, res, next) {
+  let result = testLetter(req.body.letter)
+  if (result) {
+    let isFinished = checkFinishedLvl3()
+    if(isFinished) {
+      currentLvl = 4
+    }
+    res.setHeader('Content-Type', 'application/json')
+    res.send(JSON.stringify({res: true, state: currentStateLvl3, finished: isFinished}))
+  } else {
+    resetLvl3()
+    res.setHeader('Content-Type', 'application/json')
+    res.send(JSON.stringify({res: false, state: currentStateLvl3, finished: false}))
+  }
+})
+
+function checkFinishedLvl3 () {
+  // TODO Parcourir les elements et vérifier s'ils sont tous a played
+}
+
+function testLetter (char) {
+  if (lettersState[char] && lettersState[char].played === false) {
+    // Letter is valid !!
+    let letter = lettersState[char]
+    for (let i = 0; i < letter.pos.length; i++) {
+      // TODO change char at
+      // currentStateLvl3.setCharAt(letter.pos[i], char)
+    }
+    lettersState[char].played = true
+    return true
+  }
+  return false
+}
+
+function resetLvl3 () {
+  currentStateLvl3 = defaultStateLvl3
+  //lettersState TODO copy lettersDefault
+}
+
+router.get('/password', function (req, res, next) {
+  if (currentLvl < 4) {
+    res.setHeader('Content-Type', 'application/json')
+    res.send(JSON.stringify({password: 'RETOURNE FAIRE LES NIVEAU FLEMASSE, TU CROIS QUE JE ME SUIS CASSE LE CUL POUR RIEN ???'}))
+  } else {
+    res.setHeader('Content-Type', 'application/json')
+    res.send(JSON.stringify({password: 'La caverne c\'est mon tapis, et le mot de passe à indiquer au gardien est : \"Garde le cadeau, je n\'en veux pas\"'}))
+  }
+})
 
 export default router
